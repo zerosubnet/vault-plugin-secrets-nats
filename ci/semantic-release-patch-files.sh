d5c3fc26d19d68c60c7c307309ee67419f6daa16
zs-ko <zs-ko@users.noreply.github.com>
2024-06-03 00:00:46 +0200

    fix: add release comment to script

--- ci/semantic-release-patch-files.sh
+++ ci/semantic-release-patch-files.sh
@@ -11,7 +11,7 @@ for file in build/vault/plugins/vault-plugin-secrets-nats-*; do
 done
 # only use x86 for the README.md and dev/manifests/vault/vault.yaml
 export SHA256SUM=$(cat build/vault/plugins/vault-plugin-secrets-nats.sha256 | cut -d ' ' -f1)
-#sed -i "s#sha256: .*#sha256: ${SHA256SUM}#g" README.md
-#sed -i "s#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:.*#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:${NEXTVERSION}#g" README.md
-#sed -i "s#sha256: .*#sha256: ${SHA256SUM}#g" dev/manifests/vault/vault.yaml
-#sed -i "s#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:.*#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:${NEXTVERSION}#g" dev/manifests/vault/vault.yaml
+sed -i "s#sha256: .*#sha256: ${SHA256SUM}#g" README.md
+sed -i "s#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:.*#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:${NEXTVERSION}#g" README.md
+sed -i "s#sha256: .*#sha256: ${SHA256SUM}#g" dev/manifests/vault/vault.yaml
+sed -i "s#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:.*#image: ghcr.io/edgefarm/vault-plugin-secrets-nats/vault-with-nats-secrets:${NEXTVERSION}#g" dev/manifests/vault/vault.yaml
